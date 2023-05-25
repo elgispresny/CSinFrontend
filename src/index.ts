e7729a11326c70c0a94147bde6e03abacb4a39a5
Ilgiz khafizov <@ii.hafizov@yandex.ru>
2023-05-26 00:38:47 +0300

    refactoring projects tree

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import Link from "./Link";
+import DoubleLinkedList from "./DoubleLinkedList";
+
+let list = new DoubleLinkedList();
+
+list.insertForward(new Link(1));
+list.insertForward(new Link(2));
+list.insertForward(new Link(3));
+list.insertForward(new Link(4));
+
+list.displayFromFirst();
+console.log(list.first.data + " " + list.first.next.data + " " + list.first.next.next.data + " " + list.last.data + " " + list.last.previous.data);
+
+list.insertBackward(new Link(1));
+list.displayFromFirst();
+list.displayFromLast();
